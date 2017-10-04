aa4460b70e40d5683fb9bfb10de13a99e87a82f8
Wyb0 <andrewwybo@gmail.com>
2017-10-04 16:21:24 -0400

    added require for path, added working port

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var exphbrs = require('express-handlebars')
+var path = require('path')
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function(){
+    console.log("Listening on Port: " + PORT)
+})
