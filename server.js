6f0b39e1f3fc1699da1b507a6f9cb0b7f42c4368
Wyb0 <andrewwybo@gmail.com>
2017-10-05 19:13:11 -0400

    added app.use bodyparser and handlebars engine to server

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var app = express();
 
 var PORT = process.env.PORT || 8000;
 
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+
 app.listen(PORT, function(){
     console.log("Listening on Port: " + PORT)
 })
